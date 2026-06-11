f6e1ff93253a73eac4e954aecdc5a84a3774ef14
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-11 22:31:02 +0300

    Solve Problem04

--- Algorithms and Problem Solving Level 1/Problem004/Problem004/Problem004.cpp
+++ Algorithms and Problem Solving Level 1/Problem004/Problem004/Problem004.cpp
@@ -1,20 +1,53 @@
-// Problem004.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Write a program that reads the user age and license status, then prints "Hired" if the user is over 21 and has a driving license, otherwise prints "Rejected"
 #include <iostream>
+using namespace std;
+
+struct stUserInfo
+{
+	int Age;
+	bool HasDrivingLicense;
+};
+
+stUserInfo ReadUserInfo()
+{
+	stUserInfo UserInfo;
+
+	cout << "Please enter your age : ";
+	cin >> UserInfo.Age;
+
+	cout << "Do you have a driver license? Enter 1 for Yes, 0 for No : " << endl;
+	cin >> UserInfo.HasDrivingLicense;
+
+	return UserInfo;
+}
+
+bool IsUserAccepted(stUserInfo UserInfo)
+{
+
+	return (UserInfo.Age > 21 && UserInfo.HasDrivingLicense);
+
+}
+
+void PrintUserResult(stUserInfo UserInfo)
+{
+	
+	if (IsUserAccepted(UserInfo))
+	{
+		cout << "\n Hired !\n";
+	}
+	else
+	{
+		cout << "\n Rejected \n";
+	}
+
+}
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	PrintUserResult(ReadUserInfo());
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
