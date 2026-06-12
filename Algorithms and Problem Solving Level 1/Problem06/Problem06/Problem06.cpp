d922818701f0846c742121899cd488984d238f86
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-13 01:45:01 +0300

    Solve Problem06

--- Algorithms and Problem Solving Level 1/Problem06/Problem06/Problem06.cpp
+++ Algorithms and Problem Solving Level 1/Problem06/Problem06/Problem06.cpp
@@ -1,3 +1,44 @@
 // Write a program that ask the user to enter his first name and last name then print it as full name
 #include <iostream>
 using namespace std;
+
+
+struct stUserInfo
+{
+
+	string FirstName;
+	string LastName;
+
+};
+
+stUserInfo ReadName()
+{
+
+	stUserInfo userInfo;
+
+	cout << "Please enter your first name : ";
+	cin >> userInfo.FirstName;
+
+	cout << "Please enter your last name : ";
+	cin >> userInfo.LastName;
+
+	return userInfo;
+
+}
+
+string GetFullName(stUserInfo userInfo)
+{
+
+	return userInfo.FirstName + " " + userInfo.LastName;
+
+}
+
+void PrintFullName(string FullName)
+{
+	cout << "Your full name is : " << FullName << endl;
+}
+
+int main()
+{
+	PrintFullName(GetFullName(ReadName()));
+}
