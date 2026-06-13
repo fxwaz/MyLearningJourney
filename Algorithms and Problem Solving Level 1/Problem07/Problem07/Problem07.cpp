88d069458f4b33ce5517e012e98262e00b4b6919
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-13 19:47:05 +0300

    Solve Problem07

--- Algorithms and Problem Solving Level 1/Problem07/Problem07/Problem07.cpp
+++ Algorithms and Problem Solving Level 1/Problem07/Problem07/Problem07.cpp
@@ -1,1 +1,40 @@
-//
+// Write a program that asks the user to enter a number, then prints half of that number.
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int ReadNumber()
+{
+
+	int UserNumber;
+
+	cout << "Please enter a number : ";
+	cin >> UserNumber;
+
+	return UserNumber;
+}
+
+float CalculateHalfNumber(int usernumber)
+{
+	return float(usernumber) / 2;
+}
+
+void PrintResult(int number)
+{
+	string Result;
+
+	Result = "Half of " + to_string(number) + " Is : " + to_string(CalculateHalfNumber(number));
+
+	cout << Result << endl;
+}
+
+
+int main()
+{
+	
+	PrintResult(ReadNumber());
+	
+
+}
