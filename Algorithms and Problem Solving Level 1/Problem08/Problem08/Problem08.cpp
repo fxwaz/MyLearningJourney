685bf399e44268edfad4b1ab57065310e17c05b2
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-13 20:37:48 +0300

    Solve Problem08

--- Algorithms and Problem Solving Level 1/Problem08/Problem08/Problem08.cpp
+++ Algorithms and Problem Solving Level 1/Problem08/Problem08/Problem08.cpp
@@ -1,20 +1,48 @@
-// Problem08.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Write a program that asks the user to enter their mark, then prints "PASS" if the mark is >= 50, otherwise prints "FAIL".
 #include <iostream>
+using namespace std;
+
+enum enPassOrFail { Pass = 1, Fail = 2};
+
+int ReadMark()
+{
+	int Mark;
+
+	cout << "Please enter your mark : ";
+	cin >> Mark;
+
+	return Mark;
+}
+
+enPassOrFail CheckMark(int Mark)
+{
+
+	if (Mark >= 50)
+	{
+		return enPassOrFail::Pass;
+	}
+	else
+	{
+		return enPassOrFail::Fail;
+	}
+		
+}
+
+void PrintResult(int Mark)
+{
+
+	if (CheckMark(Mark) == enPassOrFail::Pass)
+	{
+		cout << "\n\nNice bro, you passed!\n\n";
+	}	
+	else
+	{
+		cout << "\n\nSorry bro, you failed\n\n";
+	}
+		
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	PrintResult(ReadMark());
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
