c2f79e49a7d59d8716e453911b86b3d015952b81
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-14 00:41:43 +0300

    Solve Problem09

--- Algorithms and Problem Solving Level 1/Problem09/Problem09/Problem09.cpp
+++ Algorithms and Problem Solving Level 1/Problem09/Problem09/Problem09.cpp
@@ -1,20 +1,41 @@
-// Problem09.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Write a program that asks the user to enter three numbers, then prints their sum.
 #include <iostream>
+using namespace std;
+
+
+void ReadNumbers(int &first, int &second, int &third)
+{
+
+	cout << "Please enter first number : ";
+	cin >> first;
+
+	cout << "Please enter second number :";
+	cin >> second;
+
+	cout << "Please enter third number :";
+	cin >> third;
+
+	
+}
+
+int CalculateNumbers(int first, int second, int third)
+{
+	return first + second + third;
+}
+
+void PrintResult(int result)
+{
+	cout << "The result is : " << result << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	int number1, number2, number3;
+
+	ReadNumbers(number1, number2, number3);
+	PrintResult(CalculateNumbers(number1, number2, number3));
+
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
