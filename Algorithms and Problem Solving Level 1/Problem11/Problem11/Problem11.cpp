82d56e52f53b9767759141ef9cbb39613355f1a3
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-14 16:47:24 +0300

    Solve Problem11

--- Algorithms and Problem Solving Level 1/Problem11/Problem11/Problem11.cpp
+++ Algorithms and Problem Solving Level 1/Problem11/Problem11/Problem11.cpp
@@ -1,20 +1,66 @@
-// Problem11.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Write a program that asks the user to enter marks for three exams then print the average and print "PASS" if the average is greater than or equal to 50, otherwise print "FAIL".
 #include <iostream>
+using namespace std;
+
+enum enPassFail { Pass = 1, Fail };
+
+
+void ReadMarks(int& mark1, int& mark2, int& mark3)
+{
+	cout << "Please enter first mark : ";
+	cin >> mark1;
+
+	cout << "Please enter second mark : ";
+	cin >> mark2;
+
+	cout << "Please enter third mark : ";
+	cin >> mark3;
+}
+
+int SumMarks(int mark1, int mark2, int mark3)
+{
+	return mark1 + mark2 + mark3;
+}
+
+float CalculateAverage(int mark1, int mark2, int mark3)
+{
+	return float(SumMarks(mark1, mark2, mark3)) / 3;
+}
+
+enPassFail CheckAverage(float average)
+{
+	if (average >= 50)
+	{
+		return enPassFail::Pass;
+	}
+	else
+	{
+		return enPassFail::Fail;
+	}
+}
+
+void PrintResult(float result)
+{
+
+	cout << "Your average is : " << result << endl;
+
+	if (CheckAverage(result) == enPassFail::Pass)
+	{
+		cout << "Pass !" << endl;
+	}
+	else
+	{
+		cout << "Fail !" << endl;
+	}
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int mark1, mark2, mark3;
+
+	ReadMarks(mark1, mark2, mark3);
+	PrintResult(CalculateAverage(mark1, mark2, mark3));
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
