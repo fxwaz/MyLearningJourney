8b119861f508f53d4c44f2f5533c77cf123433f1
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-15 22:35:04 +0300

    Solve Problem15

--- Algorithms and Problem Solving Level 1/15 - Calculate the area of a rectangle/Problem15.cpp
+++ Algorithms and Problem Solving Level 1/15 - Calculate the area of a rectangle/Problem15.cpp
@@ -1,19 +1,33 @@
 // Write a program that calculates the area of a rectangle and prints it on the screen.
-
 #include <iostream>
+using namespace std;
+
+
+void ReadLengthAndWidth(float& Length, float& Width)
+{
+	cout << "Please enter the Length : ";
+	cin >> Length;
+
+	cout << "Please enter the Width : ";
+	cin >> Width;
+}
+
+float CalculateArea(float Length, float Width)
+{
+	return Length * Width;
+}
+
+void PrintResult(float Area)
+{
+	cout << "\n Rectangle Area = " << Area << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	float Length, Width;
+
+	ReadLengthAndWidth(Length, Width);
+	PrintResult(CalculateArea(Length, Width));
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
