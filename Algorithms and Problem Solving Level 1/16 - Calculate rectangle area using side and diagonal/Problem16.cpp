b36e08b31262e236eec5ff922166c361f5e700f3
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-15 23:02:33 +0300

    Solve Problem16

--- Algorithms and Problem Solving Level 1/16 - Calculate rectangle area using side and diagonal/Problem16.cpp
+++ Algorithms and Problem Solving Level 1/16 - Calculate rectangle area using side and diagonal/Problem16.cpp
@@ -1,5 +1,32 @@
 // Write a program to calculate rectangle area through diagonal and side area of rectangle and print it on the screen
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 
+void ReadNumbers(float& side, float& diagonal)
+{
+	cout << "Please enter rectangle side : ";
+	cin >> side;
+
+	cout << "Please enter rectangle diagonal : ";
+	cin >> diagonal;
+}
+
+float CalculateRectangleAreaBySideAndDiagonal(float side, float diagonal)
+{
+	return side * sqrt(pow(diagonal, 2) - pow(side, 2));
+}
+
+void PrintResult(float result)
+{
+	cout << "Rectangle Area = " << result << endl;
+}
+
+int main()
+{
+	float side, diagonal;
+
+	ReadNumbers(side, diagonal);
+	PrintResult(CalculateRectangleAreaBySideAndDiagonal(side, diagonal));
+}
