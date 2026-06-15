bf2cc84dda3a2f33d548a804abfeb74f86aaefe7
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-15 23:57:03 +0300

    solve problem17

--- Algorithms and Problem Solving Level 1/17 - Calculate triangle area/Problem17.cpp
+++ Algorithms and Problem Solving Level 1/17 - Calculate triangle area/Problem17.cpp
@@ -1,3 +1,33 @@
 // Write a program to calculate triangle area then print it on the screen
 #include <iostream>
 using namespace std;
+
+void ReadNumbers(float& base, float& height)
+{
+
+	cout << "Please enter triangle base : ";
+	cin >> base;
+
+	cout << "Please enter triangle height : ";
+	cin >> height;
+
+}
+
+float CalculateTriangleArea(float base, float height)
+{
+	return 0.5 * (base * height);
+}
+
+void PrintResult(float result)
+{
+	cout << "\n Triangle Area = " << result << endl;
+}
+
+int main()
+{
+	float base, height;
+
+	ReadNumbers(base, height);
+	PrintResult(CalculateTriangleArea(base, height));
+
+}
