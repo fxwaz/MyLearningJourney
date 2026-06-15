37c677c2bd0cdf249ccde14bff1964400149fc78
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-16 02:46:05 +0300

    solve problem19

--- Algorithms and Problem Solving Level 1/19 - Calculate circle area through diameter/Problem19.cpp
+++ Algorithms and Problem Solving Level 1/19 - Calculate circle area through diameter/Problem19.cpp
@@ -1,2 +1,35 @@
 // Write a program to calculate circle area through diameter, then print it on the screen.
 #include <iostream>
+#include <cmath>
+using namespace std;
+
+
+float ReadDiameter()
+{
+	float Diameter;
+
+	cout << "Please enter Diameter : ";
+	cin >> Diameter;
+
+	return Diameter;
+}
+
+float CalculateCircleArea(float Diameter)
+{
+	const float PI = 3.14159265358979323846;
+
+	return (PI * pow(Diameter, 2)) / 4;
+}
+
+void PrintResult(float AreaResult)
+{
+	cout << "\n Circle area = " << AreaResult << endl;
+}
+
+int main()
+{
+
+	PrintResult(CalculateCircleArea(ReadDiameter()));
+
+}
+
