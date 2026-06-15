1d34fcaf2bc7bb431025bb8a34304a4848bf4229
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-16 00:27:28 +0300

    solve problem18

--- Algorithms and Problem Solving Level 1/18 - Calculate circle area/Problem18.cpp
+++ Algorithms and Problem Solving Level 1/18 - Calculate circle area/Problem18.cpp
@@ -1,3 +1,33 @@
 // Write a program to calculate circle area then print it on the screen
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+
+float ReadRadius()
+{
+	float Radius;
+	cout << "Please enter Radious : ";
+	cin >> Radius;
+
+	return Radius;
+}
+
+float CalculateCircleArea(float Radius)
+{
+	const float PI = 3.14159265358979323846;
+
+	return PI * pow(Radius, 2);
+}
+
+void PrintResult(float result)
+{
+	cout << "\n Circle Area = " << result << endl;
+}
+
+int main()
+{
+
+	PrintResult(CalculateCircleArea(ReadRadius()));
+
+}
