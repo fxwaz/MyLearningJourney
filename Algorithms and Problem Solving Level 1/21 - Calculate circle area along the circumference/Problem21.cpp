4cc59584ca670f01d14d2fd0888c7e024265061a
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-16 06:10:35 +0300

    solve problem21

--- Algorithms and Problem Solving Level 1/21 - Calculate circle area along the circumference/Problem21.cpp
+++ Algorithms and Problem Solving Level 1/21 - Calculate circle area along the circumference/Problem21.cpp
@@ -2,3 +2,35 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
+
+float ReadCircumference()
+{
+	float Circumference;
+
+	cout << "Please enter circle circumference : ";
+	cin >> Circumference;
+
+	return Circumference;
+}
+
+float CircleAreaByCircumference(float Circumference)
+{
+	const float PI = 3.14159265358979323846;
+
+	return pow(Circumference, 2) / (4 * PI);
+}
+
+void PrintResult(float CircleArea)
+{
+	cout << "Circle area = " << CircleArea << endl;
+}
+
+int main()
+{
+	
+
+	PrintResult(CircleAreaByCircumference(ReadCircumference()));
+
+
+}
