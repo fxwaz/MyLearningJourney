10a1ef435ee91781c6eacb486cd5d77b73b81141
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-17 00:42:42 +0300

    add problem25 file

---
+++ Algorithms and Problem Solving Level 1/25 - Validate age between 18 and 45 with while loop/Problem25.cpp
@@ -0,0 +1,3 @@
+// Write a program to ask the user to enter Age, if age is between 18 and 45 print "Valid age" otherwise print "Invalid age" and reask user to enter a valid age
+#include <iostream>
+using namespace std;
