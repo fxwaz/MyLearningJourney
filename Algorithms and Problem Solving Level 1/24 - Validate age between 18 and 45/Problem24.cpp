a6334c3b92ed220b2def0c706f5089a52a63ad4a
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-16 23:12:11 +0300

    solve problem24

--- Algorithms and Problem Solving Level 1/24 - Validate age between 18 and 45/Problem24.cpp
+++ Algorithms and Problem Solving Level 1/24 - Validate age between 18 and 45/Problem24.cpp
@@ -1,5 +1,32 @@
-// Write a program to ask the user to enter Age if age is between 18 and 45 print "Valid age" otherwise print "Invalid age"
+// Write a program to ask the user to enter Age, if age is between 18 and 45 print "Valid age" otherwise print "Invalid age"
 #include <iostream>
 using namespace std;
 
 
+int ReadUserAge()
+{
+	int UserAge;
+
+	cout << "Please enter your age : ";
+	cin >> UserAge;
+
+	return UserAge;
+}
+
+bool IsAgeInRange(int userage, int from, int to)
+{
+	return (userage >= from && userage <= to);
+}
+
+void PrintResult(int userage)
+{
+	if (IsAgeInRange(userage, 18, 45))
+		cout << "\n " << userage << " is Valid age \n";
+	else
+		cout << "\n " << userage << " is Invalid age \n";
+}
+
+int main()
+{
+	PrintResult(ReadUserAge());
+}
