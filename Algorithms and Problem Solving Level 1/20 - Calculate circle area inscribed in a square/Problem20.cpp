0f148d67d6a587def6c605eb7e70153cfc8d6f13
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-16 05:59:53 +0300

    solve problem20

--- Algorithms and Problem Solving Level 1/20 - Calculate circle area inscribed in a square/Problem20.cpp
+++ Algorithms and Problem Solving Level 1/20 - Calculate circle area inscribed in a square/Problem20.cpp
@@ -2,3 +2,31 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
+
+float ReadSquareSide()
+{
+	float Side;
+
+	cout << "Please enter square side : ";
+	cin >> Side;
+
+	return Side;
+}
+
+float CalculateCircleArea(float Side)
+{
+	const float PI = 3.14159265358979323846;
+
+	return (PI * pow(Side, 2)) / 4;
+}
+
+void PrintResult(float Result)
+{
+	cout << "Circle area = " << Result << endl;
+}
+
+int main()
+{
+	PrintResult(CalculateCircleArea(ReadSquareSide()));
+}
