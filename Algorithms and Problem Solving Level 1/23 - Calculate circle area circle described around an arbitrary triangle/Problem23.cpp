728fa5f742913de435d32086098fa94efc8d6805
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-16 22:32:41 +0300

    solve problem23

--- Algorithms and Problem Solving Level 1/23 - Calculate circle area circle described around an arbitrary triangle/Problem23.cpp
+++ Algorithms and Problem Solving Level 1/23 - Calculate circle area circle described around an arbitrary triangle/Problem23.cpp
@@ -2,3 +2,39 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
+
+void ReadNumbers(float& a, float& b, float& c)
+{
+	cout << "Please enter triangle side a : ";
+	cin >> a;
+
+	cout << "Please enter triangle base b : ";
+	cin >> b;
+
+	cout << "Please enter triangle side c : ";
+	cin >> c;
+}
+
+float CalculateCircleAreaByTriangle(float a, float b, float c)
+{
+	float p = (a + b + c) / 2;
+	const float PI = 3.14159265358979323846;
+	float Area = (a * b * c) / (4 * sqrt(p * (p - a) * (p - b) * (p - c)));
+
+	return PI * pow(Area, 2);
+
+}
+
+void PrintResult(float result)
+{
+	cout << "Circle area = " << result << endl;
+}
+
+int main()
+{
+	float a, b, c;
+
+	ReadNumbers(a, b, c);
+	PrintResult(CalculateCircleAreaByTriangle(a, b, c));
+}
