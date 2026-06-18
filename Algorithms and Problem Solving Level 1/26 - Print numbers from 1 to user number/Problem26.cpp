9f4dd234d7ebb041f509448cffc5559d892fd117
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-18 07:30:42 +0300

    solve problem26

--- Algorithms and Problem Solving Level 1/26 - Print numbers from 1 to user number/Problem26.cpp
+++ Algorithms and Problem Solving Level 1/26 - Print numbers from 1 to user number/Problem26.cpp
@@ -1,3 +1,28 @@
 // Write a program to print numbers from 1 to user number.
 #include <iostream>
 using namespace std;
+
+
+int ReadNumber()
+{
+	int UserNumber;
+
+	cout << "Please enter a number : ";
+	cin >> UserNumber;
+
+	return UserNumber;
+}
+
+void PrintNumbersFrom1ToN(int UserNumber)
+{
+	for (int i = 1; i <= UserNumber; i++)
+	{
+		cout << i << endl;
+	}
+		
+}
+
+int main()
+{
+	PrintNumbersFrom1ToN(ReadNumber());
+}
