35a5daa32df1a5219c70e51f7d9a3893815294bc
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-18 07:40:51 +0300

    add while and do-while loop solutions

--- Algorithms and Problem Solving Level 1/26 - Print numbers from 1 to user number/Problem26.cpp
+++ Algorithms and Problem Solving Level 1/26 - Print numbers from 1 to user number/Problem26.cpp
@@ -5,24 +5,57 @@ using namespace std;
 
 int ReadNumber()
 {
+
 	int UserNumber;
 
 	cout << "Please enter a number : ";
 	cin >> UserNumber;
 
 	return UserNumber;
+
 }
 
-void PrintNumbersFrom1ToN(int UserNumber)
+void PrintNumbersFrom1ToN_UsingForLoop(int UserNumber)
 {
+
 	for (int i = 1; i <= UserNumber; i++)
 	{
 		cout << i << endl;
 	}
+
+}
+
+void PrintNumbersFrom1ToN_UsingWhileLoop(int UserNumber)
+{
+
+	int i = 1;
+
+	while (i <= UserNumber)
+	{
+		cout << i << endl;
+		i++;
+	}
 		
+}
+
+void PrintNumbersFrom1ToN_UsingDoWhileLoop(int UserNumber)
+{
+
+	int i = 1;
+
+	do
+	{
+
+		cout << i << endl;
+		i++;
+
+	} while (i <= UserNumber);
+
 }
 
 int main()
 {
-	PrintNumbersFrom1ToN(ReadNumber());
+	PrintNumbersFrom1ToN_UsingForLoop(ReadNumber());
+	PrintNumbersFrom1ToN_UsingWhileLoop(ReadNumber());
+	PrintNumbersFrom1ToN_UsingDoWhileLoop(ReadNumber());
 }
