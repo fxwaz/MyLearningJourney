1d691836b9935c2cd26f16d7b7f3ce29219c7fa7
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-06-30 14:17:12 +0300

    Update

--- Algorithms and Problem Solving Level 2/18 - Encrypt and decrypt text/18 - Encrypt and decrypt text.cpp
+++ Algorithms and Problem Solving Level 2/18 - Encrypt and decrypt text/18 - Encrypt and decrypt text.cpp
@@ -1,20 +1,41 @@
-// 18 - Encrypt and decrypt text.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// write a program to read a text and encrypt it, decrypt it.
 #include <iostream>
+using namespace std;
+
+
+string ReadText(string Text)
+{
+	string UserText; 
+
+	cout << Text;
+	cin >> UserText;
+
+	return UserText;
+}
+
+int CheckLength(string UserText)
+{
+	return UserText.length();
+}
+
+string EncryptText(string Text)
+{
+	int TextLength = Text.length() - 1;
+	string NewText = "";
+
+	for (int i = 1; i <= TextLength; i++)
+	{
+		NewText = NewText + Text[0+1];
+	}
+
+	return NewText;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string Name = "Fawaz";
+
+	Name[0] = 68;
+
+	cout << Name;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
