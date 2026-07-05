062a308a89b307f6780ba8504c2f5ccb7744488c
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-07-05 12:42:20 +0300

    solve problem27

---
+++ Algorithms and Problem Solving Level 2/27 - Average of random array/27 - Average of random array.cpp
@@ -0,0 +1,87 @@
+// Write a program to fill array with max size 100 with random numbers from 1 to 100, then print average of all numbers.
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int ReadNumberBetween(string Message, int From, int To)
+{
+	int UserNumber;
+
+	do
+	{
+		cout << Message;
+		cin >> UserNumber;
+
+		if (UserNumber < From || UserNumber > To)
+			cout << "You have to enter a number between " << From << " and " << To << ".\n\n";
+
+	} while (UserNumber < From || UserNumber > To);
+
+	return UserNumber;
+}
+
+int RandomNumber(int From, int To)
+{
+	int Randomnumber = rand() % (To - From + 1) + From;
+
+	return Randomnumber;
+}
+
+void FillArrayWithRandomNumbers(int Array[], int& ArrayLength)
+{
+	ArrayLength = ReadNumberBetween("Please enter number of elements: ", 1, 100);
+
+	for (int i = 0; i < ArrayLength; i++)
+	{
+		Array[i] = RandomNumber(1, 100);
+	}
+}
+
+void PrintArray(int Array[], int ArrayLength)
+{
+
+	for (int i = 0; i < ArrayLength; i++)
+	{
+
+		cout << Array[i] << " ";
+
+	}
+
+	cout << endl;
+
+}
+
+int SumOfAllNumbersInArray(int Array[], int ArrayLength)
+{
+	int SumBox = 0;
+
+	for (int i = 0; i < ArrayLength; i++)
+	{
+		SumBox = SumBox + Array[i];
+	}
+
+	return SumBox;
+}
+
+float AverageOfArray(int Array[], int ArrayLength)
+{
+	return float(SumOfAllNumbersInArray(Array, ArrayLength)) / ArrayLength;
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int Array[100];
+	int ArrayLength;
+
+	FillArrayWithRandomNumbers(Array, ArrayLength);
+
+	cout << "\nArray Elements : ";
+	PrintArray(Array, ArrayLength);
+
+	cout << "Average of all numbers is : " << AverageOfArray(Array, ArrayLength) << endl;
+
+}
