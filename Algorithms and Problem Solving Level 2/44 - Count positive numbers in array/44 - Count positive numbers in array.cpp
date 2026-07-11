3bee70fff83e77b7dcff757b32c8200bd6312de7
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-07-11 13:29:47 +0300

    solve problem44

---
+++ Algorithms and Problem Solving Level 2/44 - Count positive numbers in array/44 - Count positive numbers in array.cpp
@@ -0,0 +1,81 @@
+// Write a program to fill array with max size 100 with random numbers from -100 to 100, then print the count of Positive numbers.
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+int ReadNumberInRange(string message, int from, int to)
+{
+	int UserNumber;
+
+	do
+	{
+		cout << message;
+		cin >> UserNumber;
+
+	} while (UserNumber < from || UserNumber > to);
+
+	return UserNumber;
+}
+
+int RandomNumber(int From, int To)
+{
+	int RandomNumber;
+
+	RandomNumber = rand() % (To - From + 1) + From;
+
+	return RandomNumber;
+}
+
+void FillArrayWithRandomNumbers(int Array[], int& ArrayLength)
+{
+	ArrayLength = ReadNumberInRange("Please enter array length from 1 to 100 : ", 1, 100);
+
+	for (int i = 0; i < ArrayLength; i++)
+	{
+		Array[i] = RandomNumber(-100, 100);
+	}
+}
+
+void PrintArray(int Array[], int ArrayLength)
+{
+	for (int i = 0; i < ArrayLength; i++)
+	{
+		cout << Array[i] << " ";
+	}
+}
+
+int CountPositiveNumbersInArray(int Array[], int ArrayLength)
+{
+	int HowManyPositive = 0;
+
+	for (int i = 0; i < ArrayLength; i++)
+	{
+		if (Array[i] > 0)
+			HowManyPositive = HowManyPositive + 1;
+	}
+
+	return HowManyPositive;
+}
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	int Array[100];
+	int ArrayLength;
+
+	FillArrayWithRandomNumbers(Array, ArrayLength);
+
+
+	cout << "\nArray Elements : ";
+	PrintArray(Array, ArrayLength);
+
+
+	cout << "\nPositive Numbers count is : ";
+	cout << CountPositiveNumbersInArray(Array, ArrayLength) << endl;
+
+
+}
