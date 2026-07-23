5a2bff8cc19aa7c5fb09de3868da9359b0fcbd1a
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-07-23 16:45:49 +0300

    Complete Lesson01

---
+++ C++ Level 2/01 - What Is Debugging and Why/01 - What Is Debugging and Why.cpp
@@ -0,0 +1,28 @@
+/*
+
+There are three types of programming errors:
+
+1. Syntax Error
+2. Runtime Error
+3. Logical Error
+
+Syntax Error:
+يصير بسبب أنه يكون فيه خطأ في كتابة الكود مثل نسيان الفاصلة المنقوطة او كتابة اسم نوع بيانات بشكل خاطئ 
+It happens when there is an error in writing the code such as forgetting a semicolon or writing a data type name incorrectly.
+
+
+Runtime Error:
+البرنامج يشتغل عادي ولكن راح يصير خطأ وهو شغال بسبب اخطاء مثل القسمة على صفر وإلخ
+The program runs normally but an error happens while it is running because of mistakes such as dividing by zero etc..
+
+
+Logical Error:
+البرنامج يشتغل عادي بدون اي رسالة خطأ لكن المُشكلة ان النتيجة تكون غير صحيحة بسبب خطأ في منطق البرنامج زي مثلاً معادلة خاطئة او شرط مو صحيح
+The program runs normally without any error message but the result is incorrect because there is a mistake in the program logic such as using a wrong formula or an incorrect condition.
+
+
+Definition of Debugging:
+هي عملية تتبع تنفيذ الكود سطر بسطر ومراقبة قيم المُتغيرات والبحث عن الاخطاء وخصوصاً الأخطاء المنطقية ثم إصلاحها 
+It is the process of tracing the execution of the code line by line, monitoring the values of variables, searching for errors, especially logical errors and then fixing them.
+
+*/
