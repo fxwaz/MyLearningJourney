c21827a995d7157415e0fcf495875695c75df52d
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-07-24 21:27:32 +0300

    Complete Lesson03

---
+++ C++ Level 2/03 - More About Breakpoints/03 - More About Breakpoints.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+    // <<<< I started debugging from here 
+
+    int Grades[5] = { 80, 90, 100, 70, 60 };
+
+    int Sum = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        Sum = Grades[i];      // I found that the Sum variable was overwritten in every iteration which caused a logical error
+        // Sum = Sum + Grades[i]
+    }
+
+    float Average = float(Sum) / 5;
+
+    cout << "Average = " << Average << endl;
+
+
+    // I can use more than one breakpoint to skip directly between different parts of the program
+
+}
