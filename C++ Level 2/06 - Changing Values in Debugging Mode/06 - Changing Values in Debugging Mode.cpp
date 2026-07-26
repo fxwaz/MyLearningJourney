bb7e60aa50a014e872fcd00e3030400792a6b66e
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-07-26 23:26:10 +0300

    Complete Lesson06

---
+++ C++ Level 2/06 - Changing Values in Debugging Mode/06 - Changing Values in Debugging Mode.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+
+int MySum(int a, int b)
+{
+	int s = 0;
+	s = a + b;
+
+	return s;
+}
+
+
+int main()
+{
+	int arr1[5] = { 200,100,50,25,30 };
+	int a, b, c;
+
+	a = 10;
+	b = 20;
+	a++;
+	++b;
+
+	c = a + b;
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+
+	for (int i = 1; i <= 5; i++)
+	{
+		cout << i << endl;
+		a = a + a * i;
+	}
+
+	c = MySum(a, b);
+
+	cout << c;
+
+	// I can change the value of variables while debugging to test different scenarios without modifying the source code
+	
+}
