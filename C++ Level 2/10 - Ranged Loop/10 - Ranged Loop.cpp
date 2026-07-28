4680f50bd3ad2d87ba1c61b7c328112b9e151e90
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-07-28 03:18:58 +0300

    Complete Lesson10

---
+++ C++ Level 2/10 - Ranged Loop/10 - Ranged Loop.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	int Array[] = { 1, 2, 3, 4, 5 };
+
+	for (int Number : Array)
+	{
+		cout << Number << endl;
+	}
+
+
+
+	/*
+	
+		for ( rangeDeclaration : rangeExpression ) 
+
+		Take each element from the rangeExpression and put it into the rangeDeclaration
+		
+
+		Example:
+		for (int Number : Numbers)
+
+		Take each number from Numbers and put it into the Number variable
+	
+	*/
+}
