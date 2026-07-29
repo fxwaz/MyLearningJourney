27dc9dc3451cdaeb0559b4aaed2d3f8b367a08cc
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-07-29 05:13:42 +0300

    Complete Lesson12

---
+++ C++ Level 2/12 - Bitwise AND Operator/12 - Bitwise AND Operator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+
+	cout << "Result : " << (12 & 25) << endl;
+
+	// The result will be 8
+	// Because we convert 12 and 25 from decimal to binary
+	// then perform the Bitwise AND operation on them
+	// 
+	// For example :
+	// 12 in binary = 00001100
+	// 25 in binary = 00011001
+	// Now let's perform the AND operation
+	/*
+	
+			00001100
+		  & 00011001
+		  ----------
+			00001000  This is the result which is equal to 8 in decimal
+	
+	
+	*/
+}
