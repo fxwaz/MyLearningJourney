5eb4c584927e46f5803bbbddadfeb465a198420b
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-07-29 05:26:05 +0300

    Complete Lesson13

---
+++ C++ Level 2/13 - Bitwise OR Operator/13 - Bitwise OR Operator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+
+	cout << "Result : " << (12 | 25) << endl;
+
+	// The result will be 29
+	// Because we convert 12 and 25 from decimal to binary
+	// then perform the Bitwise OR operation on them
+	// 
+	// For example :
+	// 12 in binary = 00001100
+	// 25 in binary = 00011001
+	// Now let's perform the OR operation
+	/*
+
+			00001100
+		  | 00011001
+		  ----------
+			00011101  This is the result which is equal to 29 in decimal
+
+
+	*/
+}
