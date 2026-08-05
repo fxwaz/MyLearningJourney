094cad8715998719f29c5a9ad38c2c898f9fad32
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-08-05 03:05:37 +0300

    Complete Lesson21

---
+++ C++ Level 2/21 - Automatic Variables/21 - Automatic Variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	// Automatic Variables
+	// The compiler automatically chooses the variable type from the assigned value 
+
+	auto a = 10;				// int type 
+	auto b = 10.5;				// double type
+	auto c = 15.5f;             // float type
+	auto d = "Fawaz Alzahrani"; // string type
+
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+}
