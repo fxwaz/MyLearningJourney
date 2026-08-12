86be289a9aed7119e8df7866d808d260e53fdb48
Fawaz Alzahrani <292439504+fxwaz@users.noreply.github.com>
2026-08-12 23:22:10 +0300

    Complete Lesson40

---
+++ C++ Level 2/40 - Pointers and Arrays/40 - Pointers and Arrays.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int arr[4] = { 10, 20, 30, 40 };
+	int* pointer = arr;
+
+	// pointer is equivalent to &arr[0];
+	// pointer + 1 is equivalent to &arr[1];
+	// pointer + 1 is equivalent to &arr[2];
+	// pointer + 1 is equivalent to &arr[3];
+
+	cout << "Addresses are : " << endl;
+	cout << pointer << endl;
+	cout << pointer + 1 << endl;
+	cout << pointer + 2 << endl;
+	cout << &pointer[3] << endl;
+	// I can do this also cout << &pointer[i] << endl;
+
+	cout << "\nValues are : " << endl;
+	cout << *(pointer) << endl;
+	cout << *(pointer + 1) << endl;
+	cout << *(pointer + 2) << endl;
+	cout << pointer[3] << endl;
+	// I can do this also cout << pointer[i] << endl;
+
+
+}
